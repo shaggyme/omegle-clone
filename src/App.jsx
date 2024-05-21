00f2bf3f038055f2639dfc67603b41b0076ae60a
shaggyme <alishaan089@gmail.com>
2024-05-21 18:48:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Body from "./Components/HomePage";
+import logo from "./assets/omegle-logo.png";
 
 function App() {
   const [joined, setJoined] = useState(false);
@@ -8,7 +9,7 @@ function App() {
     <div>
       {!joined && (
         <div style={{ textAlign: "center",  display: "flex", flexDirection: "column", alignItems: "center", height: "100vh" }}>
-          <img style={{ width: "200px", height: "200px", margin: "20px" }} src="/src/assets/omegle-logo.png" alt="omegle-logo" />
+          <img style={{ width: "200px", height: "200px", margin: "20px" }} src={logo} alt="omegle-logo" />
           <button
             style={{ padding: "10px", backgroundColor: "#1a73e8", color: "white", border: "none", borderRadius: "5px", cursor: "pointer" }}
             onClick={() => setJoined(true)}
